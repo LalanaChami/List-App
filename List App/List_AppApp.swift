6ddec29519939a45e9d471677ceb230b2287c3ce
Lalana Chamika <lalanachamika123@gmail.com>
2025-07-23 14:51:24 -0400

    Adding project initialion code

---
+++ List App/List_AppApp.swift
@@ -0,0 +1,21 @@
+//
+//  List_AppApp.swift
+//  List App
+//
+//  Created by Lalana Thanthirigama on 2025-07-23.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct List_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
